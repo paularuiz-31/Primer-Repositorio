35e9a3447209deb75172e3b520130d72bea55e72
paularuiz-31 <129335189+paularuiz-31@users.noreply.github.com>
2023-03-29 18:49:08 -0300

    cambie la palabra mundo por la palabra clase!

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
  
 		
-	System.out.println("Hola mundo");
+	System.out.println("Hola clase!");
 	
 	//variables y tipos de datos primitivos
 	
