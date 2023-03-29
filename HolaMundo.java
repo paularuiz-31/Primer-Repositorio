53a77d41cde2ea3bfa34e018957561640a89be63
paularuiz31 <paulagabrielaruiz@gmail.com>
2023-03-29 18:47:23 -0300

    archivo al repositorio remoto

---
+++ HolaMundo.java
@@ -0,0 +1,57 @@
+package PrimeraClase;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+ 
+		
+	System.out.println("Hola mundo");
+	
+	//variables y tipos de datos primitivos
+	
+	char unaletra= 'p';
+	boolean unvalorbooleano= true; //true o false
+	int miprimercontador= 66;
+	double unvalor= 1.68;
+	float otrovalor= 2.344f;
+	
+	System.out.println(unaletra);
+	System.out.println(miprimercontador);
+	
+	
+	// operaciones y expresiones
+	
+	int suma= 10 + 20;
+	int resta=15 - 12;
+	int mult=10 * 3;
+	int div=8 / 3;
+	int modulo= 2 & 3;
+	
+	System.out.println(suma);
+	System.out.println(resta);
+    
+	
+	//Ejemlos booleanos
+	
+	boolean unboleano= 10 < 25; 
+	
+	System.out.println(unboleano); 
+	
+	//o tambien se puede poner de la siguiente manera para no cambiar de nombre
+	
+	unboleano=15<=20;
+	System.out.println(unboleano);	
+	
+	//o asi si quieres cambiar de noombre
+	boolean otrobooleano= 50 > 100; 
+	
+	System.out.println(otrobooleano);
+		
+	
+	
+	
+	
+	
+	}
+
+}
