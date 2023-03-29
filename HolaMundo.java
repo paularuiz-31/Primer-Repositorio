bf0815ca8398126e1ccb6cc485d70a1bb1ff14a6
paularuiz-31 <129335189+paularuiz-31@users.noreply.github.com>
2023-03-29 19:05:27 -0300

    agrege la palabra chau clase!

--- HolaMundo.java
+++ HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
  
 		
-	System.out.println("Hola clase!");
+	System.out.println("Hola clase! chau clase!");
 	
 	//variables y tipos de datos primitivos
 	
